8dcea333f9645c75082435c4c8c2dabda2b7a643
washer83 <washer8394@gmail.com>
2024-06-19 00:04:58 -0400

    added image for shadow, added max mage preset

--- src/defaultGearSets.js
+++ src/defaultGearSets.js
@@ -132,7 +132,69 @@ const defaultGearSets = [
                 id: 28310
             }
         }
+    },
+    {
+        name: "Max Mage",
+        equipment: {
+            cape: {
+                name: "Imbued guthix cape",
+                slot: "cape",
+                bonuses: [0, 0, 0, 15, 0, 3, 3, 3, 15, 0, 0, 0, 2, 0],
+                id: 21793
+            },
+            head: {
+                name: "Ancestral hat",
+                slot: "head",
+                bonuses: [0, 0, 0, 8, -2, 12, 11, 13, 5, 0, 0, 0, 3, 0],
+                id: 21018
+            },
+            neck: {
+                name: "Occult necklace",
+                slot: "neck",
+                bonuses: [0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 5, 2],
+                id: 12002
+            },
+            weapon: {
+                name: "Tumeken's shadow",
+                slot: "2h",
+                bonuses: [0, 0, 0, 35, 0, 0, 0, 0, 20, 0, 0, 0, 0, 1],
+                id: 27275,
+                category: "Powered Staff",
+                speed: 5
+            },
+            body: {
+                name: "Ancestral robe top",
+                slot: "body",
+                bonuses: [0, 0, 0, 35, -8, 42, 31, 51, 28, 0, 0, 0, 3, 0],
+                id: 21021
+            },
+            legs: {
+                name: "Ancestral robe bottom",
+                slot: "legs",
+                bonuses: [0, 0, 0, 26, -7, 27, 24, 30, 20, 0, 0, 0, 3, 0],
+                id: 21024
+            },
+            hands: {
+                name: "Tormented bracelet",
+                slot: "hands",
+                bonuses: [0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 5, 2],
+                id: 19544
+            },
+            feet: {
+                name: "Eternal boots",
+                slot: "feet",
+                bonuses: [0, 0, 0, 8, 0, 5, 5, 5, 8, 5, 0, 0, 1, 0],
+                id: 13235
+            },
+            ring: {
+                name: "Magus ring",
+                slot: "ring",
+                bonuses: [0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0],
+                id: 28313
+            }
+        }
     }
+    
 ];
 
 export default defaultGearSets;
